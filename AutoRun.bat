edb839a09155ea0c55cfef030d5bbc66ecf06454
KuroShion <66388008+EvoTeamMalaysia@users.noreply.github.com>
2024-03-18 16:17:24 +0800

    Update AutoRun.bat

--- AutoRun.bat
+++ AutoRun.bat
@@ -1,19 +1,20 @@
 @echo off
 
-#Script By K3r3
+#Script By EvoTeamMalaysia
+
 REM Start HWiNFO Sensors
-start "" "C:\Path\To\HWiNFO64.exe" /SENSORSONLY
+start "" "F:\Test Copy\HWiNFO64.exe" /SENSORSONLY
 
 REM Wait for HWiNFO Sensors to initialize (adjust the timeout as needed)
 timeout /t 5
 
 REM Start FurMark with P1080 preset benchmark
-start "" "C:\Path\To\FurMark.exe" /benchmark /preset:P1080
+start "" "F:\Test Copy\FurMark_win64\FurMark.exe" /benchmark /preset:P1080
 
 REM Wait for FurMark to initialize (adjust the timeout as needed)
 timeout /t 10
 
 REM Start Prime95 for stress testing
-start "" "C:\Path\To\prime95.exe"
+start "" "F:\Test Copy\p95v308b17.win64\prime95.exe"
 
 echo All tests started successfully.
